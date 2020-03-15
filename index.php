4544e836fcdfeb888483fc10105326ea81e1c1e5
Emmanuel Asinyo <34837163+emmanueldubien@users.noreply.github.com>
2020-03-14 21:56:42 -0400

    modified

--- index.php
+++ index.php
@@ -211,8 +211,8 @@ if(isset($_POST['submit']))
                       <li> <a href="Product.php?value=Academic%20and%20Professional"> Academic & Professional </a> </li>
                       <li> <a href="Product.php?value=Biographies%20and%20Auto%20Biographies"> Biographies & Auto Biographies </a> </li>
                       <li> <a href="Product.php?value=Children%20and%20Teens"> Children & Teens </a> </li>
-                      <li> <a href="Product.php?value=Regional%20Books"> Regional Books </a> </li>
-                      <li> <a href="Product.php?value=Business%20and%20Management"> Business & Management </a> </li>
+<!--                       <li> <a href="Product.php?value=Regional%20Books"> Regional Books </a> </li>
+ -->                      <li> <a href="Product.php?value=Business%20and%20Management"> Business & Management </a> </li>
                       <li> <a href="Product.php?value=Health%20and%20Cooking"> Health and Cooking </a> </li>
 
                   </ul>
